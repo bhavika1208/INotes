deb4df8746cb473bcde9498e4328893fb3effec3
Sagar Desai <74409690+Sagardesai13@users.noreply.github.com>
2021-11-11 21:23:38 +0530

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,60 @@
+
+import './App.css';
+import { Navbar } from './Components/Navbar';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+import { Home } from './Components/Home';
+import { About } from './Components/About';
+import NoteState from './Context/Notes/NoteState';
+import Signup from './Components/Signup';
+import Login from './Components/Login';
+import Alert from './Components/Alert';
+import React, { useState } from 'react';
+
+
+function App() {
+  const [alert, setAlert] = useState(null);
+
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type
+    })
+
+    setTimeout(() => {
+      setAlert(null);
+    }, 1500);
+  }
+
+  return (
+    <>
+      <NoteState>
+        <Router>
+          <Navbar />
+          <Alert alert={alert} />
+          <div className="container">
+            <Switch>
+              <Route exact path="/">
+                <Home showAlert={showAlert}/>
+              </Route>
+              <Route exact path="/about">
+                <About />
+              </Route>
+              <Route exact path="/login">
+                <Login showAlert={showAlert}/>
+              </Route>
+              <Route exact path="/signup">
+                <Signup showAlert={showAlert}/>
+              </Route>
+            </Switch>
+          </div>
+        </Router>
+      </NoteState>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Components/About.js
@@ -0,0 +1,50 @@
+import React from 'react'
+
+export const About = () => {
+    return (
+        <div className="container">
+            <h1 className="my-3 text-center" >About Us</h1>
+            <div className="accordion" id="accordionExample">
+                <div className="accordion-item">
+                    <h2 className="accordion-header" id="headingOne">
+                        <button className="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                            <strong>Add Your Notes </strong>
+                        </button>
+                    </h2>
+                    <div id="collapseOne" className="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+                        <div className="accordion-body">
+                            MyNotebook gives you a way to add, update and delete your notes quickly and efficiently.
+
+                        </div>
+                    </div>
+                </div>
+                <div className="accordion-item">
+                    <h2 className="accordion-header" id="headingTwo">
+                        <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                            <strong>Free to use </strong>
+                        </button>
+                    </h2>
+                    <div id="collapseTwo" className="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
+                        <div className="accordion-body">
+                            Mynotebook is a free tool that provides instant manipulations operations for your notes.
+                        </div>
+                    </div>
+                </div>
+                <div className="accordion-item">
+                    <h2 className="accordion-header" id="headingThree">
+                        <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                            <strong>Browser Compatible </strong>
+                        </button>
+                    </h2>
+                    <div id="collapseThree" className="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
+                        <div className="accordion-body" >
+                            Mynotebook works in any web browsers such as Chrome, Firefox, Internet Explorer, Safari, Opera.
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    )
+}
